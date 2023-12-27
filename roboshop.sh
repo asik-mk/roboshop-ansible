87aab9b071b5ce99e3c85572da28d155ce4a3ec1
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-27 21:36:18 +0530

    :Master server

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-ID=$(id -u)
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-DN="mohammedasik.shop"
-AMI="ami-03265a0778a880afb"
-SG="sg-0a7b5d6d0aaba9852"
 
-if [ $ID -ne 0 ]
-then
-    echo " Switch to root user "
-    exit 1;
-else
-    echo " SIT TIGHT "
-fi
+AMI=ami-03265a0778a880afb
+SG_ID=sg-0a7b5d6d0aaba9852 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z05281403KGB5KOKKHZHT # replace your zone ID
+DN="mohammedasik.shop"
 
 for i in "${INSTANCES[@]}"
 do
-    echo "Instance is : $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t2.micro"
@@ -23,24 +15,26 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0a7b5d6d0aaba9852 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 
+    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
-    --hosted-zone-id Z05281403KGB5KOKKHZHT \
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-        "Comment": "Creating a record set"
+        "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
-        "Action"              : "CREATE"
+        "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
             "Name"              : "'$i'.'$DN'"
             ,"Type"             : "A"
-            ,"TTL"              : 0
+            ,"TTL"              : 1
             ,"ResourceRecords"  : [{
                 "Value"         : "'$IP_ADDRESS'"
             }]
         }
         }]
     }
-    ' &>> /tmp/logfile.txt
+        '
 done
