5442dbd4c18ec9d4d28fdda27e91ddd21032d85d
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-27 20:24:22 +0530

    :Master server

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 ID=$(id -u)
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 DN="mohammedasik.shop"
+AMI="ami-03265a0778a880afb"
+SG="sg-0a7b5d6d0aaba9852"
 
 if [ $ID -ne 0 ]
 then
@@ -14,13 +16,14 @@ fi
 for i in "${INSTANCES[@]}"
 do
     echo "Instance is : $i"
-    IPADDRESS=$(aws ec2 run-instances \
-        --image-id ami-03265a0778a880afb \
-        --instance-type t2.micro \
-        --count 1 \
-        --security-group-ids sg-0a7b5d6d0aaba9852 \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=$i}]" \
-        --query 'Instances[0].PrivateIpAddress' --output text)
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t2.micro"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id Z05281403KGB5KOKKHZHT \
